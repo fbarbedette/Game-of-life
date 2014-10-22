e49a8ac946c6a2c17c110abc7e4814c2c2653feb
Florian Barbedette <florian.barbedette@gmail.com>
2014-10-22 15:32:11 -0500

    Added - Game of life project initialization

---
+++ src/gameOfLife/GameOfLife.java
@@ -0,0 +1,11 @@
+package gameOfLife;
+
+/**
+ * Created by fbarbedette on 10/22/14.
+ */
+public class GameOfLife {
+
+    public static void main (String[] args) {
+        System.out.println("start of the game");
+    }
+}
---
+++ test/gameOfLife/GameOfLifeTest.java
@@ -0,0 +1,15 @@
+package gameOfLife;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by fbarbedette on 10/22/14.
+ */
+public class GameOfLifeTest {
+
+    @Test
+    public void firstTest() {
+        Assert.assertEquals(0,0);
+    }
+}
